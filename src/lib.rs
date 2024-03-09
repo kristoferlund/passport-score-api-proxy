13606f344e434e19ae6b79d725140af495dd0c0b
Kristofer <kristofer@fmckl.se>
2024-03-09 21:53:11 +0100

    Add support for API get query

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,21 @@
 use worker::*;
 
-const PASSPORT_API_URL: &str = "https://api.scorer.gitcoin.co/registry/submit-passport";
+const SUBMIT_URL: &str = "https://api.scorer.gitcoin.co/registry/submit-passport";
+const GET_URL: &str = "https://api.scorer.gitcoin.co/registry/score";
+
+fn get_secrets(ctx: &RouteContext<()>) -> worker::Result<(String, String)> {
+    let api_key = ctx.secret("PASSPORT_API_KEY")?.to_string();
+    let scorer_id = ctx.secret("PASSPORT_SCORER_ID")?.to_string();
+    Ok((api_key, scorer_id))
+}
 
 #[event(fetch)]
 pub async fn main(req: Request, env: Env, _ctx: worker::Context) -> worker::Result<Response> {
     let router = Router::new();
     router
-        .get_async("/:address", |_req, ctx| async move {
+        .get_async("/submit/:address", |_, ctx| async move {
             if let Some(address) = ctx.param("address") {
-                let api_key = ctx.secret("PASSPORT_API_KEY")?.to_string();
-                let scorer_id = ctx.secret("PASSPORT_SCORER_ID")?.to_string();
+                let (api_key, scorer_id) = get_secrets(&ctx)?;
 
                 let mut headers: Headers = Headers::new();
                 headers.append("X-API-KEY", &api_key)?;
@@ -26,10 +32,26 @@ pub async fn main(req: Request, env: Env, _ctx: worker::Context) -> worker::Resu
                 init.with_body(Some(body.into()));
                 init.with_headers(headers);
 
-                let request = Request::new_with_init(PASSPORT_API_URL, &init)?;
+                let request = Request::new_with_init(SUBMIT_URL, &init)?;
                 return Fetch::Request(request).send().await;
             }
+            Response::error("Bad Request", 400)
+        })
+        .get_async("/get/:address", |_req, ctx| async move {
+            if let Some(address) = ctx.param("address") {
+                let (api_key, scorer_id) = get_secrets(&ctx)?;
 
+                let mut headers: Headers = Headers::new();
+                headers.append("X-API-KEY", &api_key)?;
+
+                let url = format!("{}/{}/{}", GET_URL, scorer_id, address);
+                let mut init = RequestInit::new();
+                init.with_method(Method::Get);
+                init.with_headers(headers);
+
+                let request = Request::new_with_init(&url, &init)?;
+                return Fetch::Request(request).send().await;
+            }
             Response::error("Bad Request", 400)
         })
         .run(req, env)
